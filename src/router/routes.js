d06f63c828fe906f01412ce236cfe857108b42f9
Spiralis-Solutions <krisstjohn96@gmail.com>
2018-06-13 10:42:55 -0700

    f

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,6 @@
 export default [
   { path: '/', component: () => import('pages/index') },
-
+  { path: '/invoice', component: () => import('pages/invoice') },
   { // Always leave this as last one
     path: '*',
     component: () => import('pages/404')
