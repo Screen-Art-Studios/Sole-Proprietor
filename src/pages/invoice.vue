575a06c06ba88c77cf45693bef46c72f290f3838
Spiralis-Solutions <krisstjohn96@gmail.com>
2018-06-13 14:45:06 -0700

    f

--- src/pages/invoice.vue
+++ src/pages/invoice.vue
@@ -42,7 +42,7 @@ export default {
   name: 'invoice',
   data () {
     return {
-      invoiceStyle: 'simple'
+      invoiceStyle: 'natural'
     }
   },
   computed: {
@@ -50,14 +50,16 @@ export default {
       let vue = this
       return {
         simpleHeader: vue.invoiceStyle === 'simple',
-        cleanHeader: vue.invoiceStyle === 'clean'
+        darkHeader: vue.invoiceStyle === 'dark',
+        naturalHeader: vue.invoiceStyle === 'natural'
       }
     },
     mainLogic () {
       let vue = this
       return {
         simpleMain: vue.invoiceStyle === 'simple',
-        cleanMain: vue.invoiceStyle === 'clean'
+        darkMain: vue.invoiceStyle === 'dark',
+        naturalMain: vue.invoiceStyle === 'natural'
       }
     }
   }
@@ -259,5 +261,428 @@ export default {
     font-size: .8em;
     font-weight: 300;
     text-align: center;
+  }
+  /*DARK INVOICE*/
+
+  .darkMain {
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(40, 25px);
+    background-image: url('../assets/noise2.jpg');
+  }
+
+  .darkHeader {
+    background-image: url('../assets/noise.png');
+    height: 150px;
+    box-shadow: 0px 1px 10px #e20053;
+    grid-column-start: 1;
+    grid-column-end: 9;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(5, 20px);
+  }
+  .darkHeader img {
+    width: 100px;
+    height: 100px;
+    grid-column-start: 1;
+    grid-column-end: 3;
+    grid-row: 5;
+    margin-top: 15px;
+    background-color: white;
+    border-radius: 50%;
+  }
+  .darkHeader h1 {
+    opacity: .5;
+    font-size: 2.3em;
+    text-align: center;
+    font-weight: 300;
+    grid-column-start: 5;
+    grid-column-end: 7;
+    grid-row: 1;
+    color: white;
+  }
+  .darkHeader .name, .darkHeader .address, .darkHeader .citystatezip, .darkHeader .phone, .darkHeader .email, .darkHeader .website {
+    grid-column-start: 3;
+    grid-column-end: 5;
+    font-size: .8em;
+    text-align: center;
+    margin-top: 20px;
+    color: white;
+    opacity: .5;
+  }
+  .darkHeader .name {
+    font-size: 1.8em;
+    grid-column-start: 1;
+    grid-column-end: 4;
+    grid-row: 1;
+    text-align: left;
+    margin-left: 5%;
+    margin-top: 20px;
+    color: white;
+  }
+  .darkHeader .num {
+    grid-row: 5;
+    margin-left: 5%;
+    grid-column-start: 5;
+    grid-column-end: 7;
+    color: white;
+  }
+  .darkHeader .invoiceid {
+    grid-row: 6;
+    margin: 15%;
+    grid-column: 5;
+    color: white;
+  }
+  .darkMain h2 {
+    grid-row: 9;
+    font-size: 1em;
+    grid-column-start: 2;
+    grid-column-end: 4;
+    color: #e20053;
+    font-weight: 300;
+  }
+  .darkMain .toname, .darkMain .toaddress, .darkMain .tocitystatezip {
+    grid-column-start: 2;
+    grid-column-end: 5;
+    font-weight: 300;
+  }
+  .darkMain .toname {
+    grid-row: 11;
+  }
+  .darkMain .toaddress {
+    grid-row: 12;
+  }
+  .darkMain .tocitystatezip {
+    grid-row-start: 13;
+    grid-row-end: 15;
+  }
+  .darkMain .date {
+    grid-row: 9;
+    grid-column-start: 5;
+    grid-column-end: 7;
+  }
+  .darkMain .issued {
+    grid-row: 11;
+    grid-column-start: 5;
+    grid-column-end: 7;
+    font-weight: 300;
+  }
+  .darkMain .notes {
+    grid-row: 12;
+    grid-column-start: 5;
+    grid-column-end: 7;
+  }
+  .darkMain .table {
+    width: 90%;
+    margin-left: 5%;
+    grid-column-start: 1;
+    grid-column-end: 9;
+    grid-row-start: 18;
+    grid-row-end: 30;
+    background-color: white;
+    border: 1px solid #e20053;
+  }
+  .darkMain .desc {
+    grid-row: 17;
+    line-height: 0px;
+  }
+  .darkMain .qty {
+    grid-row: 17;
+    grid-column-start: 5;
+    grid-column-end: 7;
+    line-height: 0px;
+    text-align: center;
+  }
+  .darkMain .amount {
+    grid-row: 17;
+    grid-column: 7;
+    grid-column-end: 9;
+    line-height: 0px;
+    text-align: center;
+    width: 90%;
+  }
+   .darkMain .descrip {
+    display: grid;
+    grid-column-start: 1;
+    grid-column-end: 5;
+    grid-row-start: 18;
+    grid-row-end: 30;
+    border-right: 1px solid #e20053;
+  }
+  .darkMain .quant {
+    grid-column-start: 7;
+    grid-column-end: 5;
+    grid-row-start: 18;
+    grid-row-end: 30;
+    border-right: 1px solid #e20053;
+  }
+  .darkMain .subtotal {
+    grid-row: 32;
+    grid-column-start: 2;
+    grid-column-end: 7;
+  }
+  .darkMain .sub {
+    line-height: 50px;
+    grid-row: 32;
+    text-align: center;
+    grid-column-start: 7;
+    grid-column-end: 10;
+    font-weight: 300;
+  }
+  .darkMain .tax {
+    grid-row: 33;
+  }
+  .darkMain .taxammt {
+    line-height: 50px;
+    grid-row: 33;
+    text-align: center;
+    grid-column-start: 7;
+    grid-column-end: 10;
+    font-weight: 300;
+  }
+  .darkMain .final {
+    grid-row: 34;
+    font-weight: 400;
+  }
+  .darkMain .total {
+    line-height: 50px;
+    grid-row: 34;
+    text-align: center;
+    grid-column-start: 7;
+    grid-column-end: 10;
+    font-weight: 400;
+    font-size: 1.2em;
+    color: #e20053;
+  }
+  .darkMain .thanks {
+    grid-row: 35;
+    grid-column-start: 1;
+    grid-column-end: 11;
+    text-align: center;
+    font-weight: 300;
+    font-size: 1.3em;
+  }
+  .darkMain h4 {
+    grid-row: 39;
+    grid-column-start: 1;
+    grid-column-end: 11;
+    font-size: .8em;
+    font-weight: 300;
+    text-align: center;
+  }
+
+  /*NATURAL INVOICE*/
+  .naturalMain {
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(40, 25px);
+    background-color: #f2e9a9;
+  }
+
+  .naturalHeader {
+    background: linear-gradient(#87b55a, #55912a);
+    height: 150px;
+    grid-column-start: 1;
+    grid-column-end: 9;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(5, 20px);
+  }
+  .naturalHeader img {
+    width: 100px;
+    height: 100px;
+    grid-column-start: 1;
+    grid-column-end: 3;
+    grid-row: 1;
+    margin: 10%;
+    background-color: white;
+    border-radius: 50%;
+  }
+  .naturalHeader h1 {
+    font-size: 2.3em;
+    text-align: center;
+    font-weight: 300;
+    grid-column-start: 5;
+    grid-column-end: 7;
+    grid-row: 1;
+    color: #fff8c9;
+  }
+  .naturalHeader .name, .naturalHeader .address, .naturalHeader .citystatezip, .naturalHeader .phone, .naturalHeader .email, .naturalHeader .website {
+    grid-column-start: 3;
+    grid-column-end: 5;
+    font-size: .8em;
+    text-align: left;
+    margin-top: 15px;
+    color: #fff8c9;
+    font-weight: 400;
+    opacity: 1;
+  }
+  .naturalHeader .name {
+    font-size: 1em;
+    grid-column-start: 1;
+    grid-column-end: 4;
+    grid-row: 6;
+    text-align: left;
+    margin-left: 5%;
+    margin-top: 20px;
+    color: #fff8c9;
+  }
+  .naturalHeader .num {
+    grid-row: 5;
+    margin-left: 5%;
+    grid-column-start: 5;
+    grid-column-end: 7;
+    color: #fff8c9;
+  }
+  .naturalHeader .invoiceid {
+    grid-row: 6;
+    margin: 15%;
+    grid-column: 5;
+    color: #fff8c9;
+  }
+  .naturalMain h2 {
+    grid-row: 9;
+    font-size: 1em;
+    grid-column-start: 2;
+    grid-column-end: 4;
+    color: #55912a;
+    font-weight: 400;
+  }
+  .naturalMain .toname, .naturalMain .toaddress, .naturalMain .tocitystatezip {
+    grid-column-start: 2;
+    grid-column-end: 5;
+    font-weight: 400;
+    color: #755c27;
+  }
+  .naturalMain .toname {
+    grid-row: 11;
+  }
+  .naturalMain .toaddress {
+    grid-row: 12;
+  }
+  .naturalMain .tocitystatezip {
+    grid-row-start: 13;
+    grid-row-end: 15;
+  }
+  .naturalMain .date {
+    grid-row: 9;
+    grid-column-start: 5;
+    grid-column-end: 7;
+  }
+  .naturalMain .issued {
+    grid-row: 11;
+    grid-column-start: 5;
+    grid-column-end: 7;
+    font-weight: 400;
+    color: #755c27;
+  }
+  .naturalMain .notes {
+    grid-row: 12;
+    grid-column-start: 5;
+    grid-column-end: 7;
+  }
+  .naturalMain .table {
+    width: 90%;
+    margin-left: 5%;
+    grid-column-start: 1;
+    grid-column-end: 9;
+    grid-row-start: 18;
+    grid-row-end: 30;
+    background-color: #fff8c9;
+    border: 1px solid #55912a;
+  }
+  .naturalMain .desc {
+    grid-row: 17;
+    line-height: 0px;
+  }
+  .naturalMain .qty {
+    grid-row: 17;
+    grid-column-start: 5;
+    grid-column-end: 7;
+    line-height: 0px;
+    text-align: center;
+  }
+  .naturalMain .amount {
+    grid-row: 17;
+    grid-column: 7;
+    grid-column-end: 9;
+    line-height: 0px;
+    text-align: center;
+    width: 90%;
+  }
+   .naturalMain .descrip {
+    display: grid;
+    grid-column-start: 1;
+    grid-column-end: 5;
+    grid-row-start: 18;
+    grid-row-end: 30;
+    border-right: 1px solid #55912a;
+  }
+  .naturalMain .quant {
+    grid-column-start: 7;
+    grid-column-end: 5;
+    grid-row-start: 18;
+    grid-row-end: 30;
+    border-right: 1px solid #55912a;
+  }
+  .naturalMain .subtotal {
+    grid-row: 32;
+    grid-column-start: 2;
+    grid-column-end: 7;
+  }
+  .naturalMain .sub {
+    line-height: 50px;
+    grid-row: 32;
+    text-align: center;
+    grid-column-start: 7;
+    grid-column-end: 10;
+    font-weight: 400;
+    color: #755c27;
+  }
+  .naturalMain .tax {
+    grid-row: 33;
+  }
+  .naturalMain .taxammt {
+    line-height: 50px;
+    grid-row: 33;
+    text-align: center;
+    grid-column-start: 7;
+    grid-column-end: 10;
+    font-weight: 400;
+    color: #755c27;
+  }
+  .naturalMain .final {
+    grid-row: 34;
+    font-weight: 400;
+  }
+  .naturalMain .total {
+    line-height: 50px;
+    grid-row: 34;
+    text-align: center;
+    grid-column-start: 7;
+    grid-column-end: 10;
+    font-size: 1.2em;
+    color: #55912a;
+  }
+  .naturalMain .thanks {
+    grid-row: 35;
+    grid-column-start: 1;
+    grid-column-end: 11;
+    text-align: center;
+    font-size: 1.3em;
+    font-weight: 400;
+    color: #755c27;
+  }
+  .naturalMain h4 {
+    grid-row: 39;
+    grid-column-start: 1;
+    grid-column-end: 11;
+    font-size: .8em;
+    font-weight: 300;
+    text-align: center
   }
 </style>
